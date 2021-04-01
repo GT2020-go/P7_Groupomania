026ed6836f982ba9b4cc81c1d1bf7cfb7f5e0d15
Gaetan Thereau <gaetan.thereau@gmail.com>
2021-04-01 16:25:34 +0200

    Create first table with Sequelize CLI

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,19 +1,25 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
-
-require("mongoose-type-email");
-mongoose.SchemaTypes.Email.defaults.message = "Email address is invalid";
-
-const userSchema = mongoose.Schema({
-  email: {
-    type: mongoose.SchemaTypes.Email,
-    correctTld: true,
-    required: true,
-    unique: true,
-  },
-  password: { type: String, required: true },
-});
-
-userSchema.plugin(uniqueValidator);
-
-module.exports = mongoose.model("User", userSchema);
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
