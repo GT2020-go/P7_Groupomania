1c5e97867b0340bc471e390150c4aaa41e40c4e3
Gaetan Thereau <gaetan.thereau@gmail.com>
2021-12-07 11:28:23 +0100

    Update store to persist on refresh

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -8,12 +8,17 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Provider } from "react-redux";
-import store from "./store";
+// import store from "./store";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistedStore } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistedStore}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- frontend/app/src/store.js
+++ frontend/app/src/store.js
@@ -4,11 +4,28 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
+//persist Store
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+export const store = createStore(
+  persistedReducer,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
-export default store;
+// export default store;
+
+export const persistedStore = persistStore(store);
